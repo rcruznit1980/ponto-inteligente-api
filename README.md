c3e1ad16a724e38ed58ab97f6f5dd3abe1de9e9a
Rodrigo Almeida <rodrigo@namastetecnologia.com.br>
2019-07-22 10:01:17 -0300

    Controller de empresas

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 [![Build Status](https://travis-ci.org/rcruznit1980/ponto-inteligente-api.svg?branch=master)](https://travis-ci.org/rcruznit1980/ponto-inteligente-api)
 # Ponto Inteligente
 API do sistema Ponto Inteligente
+
+#Classe Principal
+PontoInteligenteApplication
+
+#Cadastro de Pessoa Física
+Lorem ipsum leo nostra donec egestas justo, dapibus curabitur volutpat euismod. ullamcorper velit morbi pellentesque aenean accumsan ultrices amet, ad aenean orci gravida fringilla a venenatis, dapibus turpis egestas tellus nisl hac. conubia velit netus ultrices curabitur est non leo, nostra ante semper ut in elit et malesuada, eu elit scelerisque augue sagittis erat. mollis cras sagittis leo dictum rhoncus vestibulum iaculis euismod sagittis, neque taciti tristique quis aptent imperdiet ornare donec
+
+#Cadastro de Pessoa Jurídica
+
+Felis consectetur lacus gravida inceptos justo aliquam convallis vulputate, ultricies duis ante quis vel justo senectus ad, proin maecenas orci condimentum tortor purus augue. nec nibh consequat turpis per ullamcorper a dui, netus aliquam senectus cursus ullamcorper ut ullamcorper consequat, amet lorem et nibh rutrum maecenas aliquam, massa aliquet conubia condimentum proin
---
+++ src/test/java/br/com/namaste/pontointeligente/api/controllers/EmpresaControllerTest.java
@@ -0,0 +1,81 @@
+package br.com.namaste.pontointeligente.api.controllers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import br.com.namaste.pontointeligente.api.model.Empresa;
+import br.com.namaste.pontointeligente.api.services.EmpresaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class EmpresaControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private EmpresaService empresaService;
+	
+	private static final String BUSCAR_EMPRESA_CNPJ_URL = "/api/empresas/cnpj/";
+	private static final Long ID = Long.valueOf(1);
+	private static final String CNPJ = "36496379000110";
+	private static final String RAZAO_SOCIAL = "Empresa XYZ";
+	
+	@Test
+	public void testBuscarEmpresaCnpjInvalido() throws Exception{
+		BDDMockito.given(this.empresaService.buscarPorCNPJ(Mockito.anyString())).willReturn(Optional.empty());
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_CNPJ_URL + CNPJ).accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.errors").value("Empresa não encontrada para o CNPJ" + CNPJ));
+		
+	}
+	
+	@Test
+	public void testBuscarEmpresaCnpjValido() throws Exception{
+		BDDMockito.given(this.empresaService.buscarPorCNPJ(Mockito.anyString())).willReturn(Optional.of(this.obterDadosEmpresa()));
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_CNPJ_URL + CNPJ)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.id").value(ID))
+			.andExpect(jsonPath("$.data.razaoSocial", equalTo(RAZAO_SOCIAL)))
+			.andExpect(jsonPath("$.data.razaoSocial", equalTo(CNPJ)))
+			.andExpect(jsonPath("$.errors").isEmpty());
+	}
+	
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setId(ID);
+		empresa.setRazaoSocial(RAZAO_SOCIAL);
+		empresa.setCnpj(CNPJ);
+		
+		return empresa;
+		
+	}
+	
+	
+	
+	
+	
+
+}
